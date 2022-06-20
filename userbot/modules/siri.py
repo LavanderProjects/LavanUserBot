aacef7ac9677fb97585c13142f30e708bd6a1be5
SakirBey1 <sakirhack81@gmail.com>
2022-06-21 00:07:14 +0300

    Update siri.py

--- userbot/modules/siri.py
+++ userbot/modules/siri.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# LavanUserBot - Ber4tbey
+# LavanUserBot - Ber4tbey - SakirBey1
 #
 from telethon import events 
 import time 
@@ -14,9 +14,9 @@ from userbot.events import register
 @register(outgoing=True,pattern="^.[Ss]iri")
 
 async def komut(event):
-        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Siri Yazmak Yerine__ `.owen` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/OwenUserBot/65 \n UserBot Kanalı: @OwenUserBot\nPlugin Kanalı: @OwenPlugin")
+        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Siri Yazmak Yerine__ `.lavan` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/LavanUserBot \n UserBot Kanalı: @LavanUserBot\nPlugin Kanalı: @LavanPlugin")
 
 @register(outgoing=True,pattern="^.[Ee]pic")
 
 async def komut(event):
-        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Epic Yazmak Yerine__ `.owen` __Yazmalısın.__\n \n**Gerekli Açıklama:** https://t.me/OwenUserBot/80 \n UserBot Kanalı: @OwenUserBot\nPlugin Kanalı: @OwenPlugin")
+        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Epic Yazmak Yerine__ `.lavan` __Yazmalısın.__\n \n**Gerekli Açıklama:** https://t.me/LavanUserBot \n UserBot Kanalı: @LavanUserBot\nPlugin Kanalı: @LavanPlugin")
