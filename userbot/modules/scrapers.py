11895a708e8b51807c566cd775633d04ca691dd9
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-07-07 19:18:44 +0300

    Fix - Geçici

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -33,7 +33,7 @@ from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 from urbandict import define
 from requests import get
-from search_engine_parser import GoogleSearch
+#from search_engine_parser import GoogleSearch
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_trans_new import LANGUAGES, google_translator
@@ -398,9 +398,9 @@ async def moni(event):
         return
 
 
-@register(outgoing=True, pattern=r"^.google ?(.*)")
+"""@register(outgoing=True, pattern=r"^.google ?(.*)")
 async def gsearch(q_event):
-    """ .google  """
+ 
     match = q_event.pattern_match.group(1)
     page = findall(r"page=\d+", match)
     try:
@@ -429,7 +429,7 @@ async def gsearch(q_event):
         await q_event.client.send_message(
             BOTLOG_CHATID,
             match + "`Sözcük başarıyla Google'da aratıldı!`",
-        )
+        )"""
 
 
 @register(outgoing=True, pattern=r"^.wiki (.*)")
