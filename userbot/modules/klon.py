891b2d5b03caeb9a406c7bba6cb5fd112aa208e9
SakirBey1 <sakirhack81@gmail.com>
2022-07-23 14:01:00 +0300

    Update klon.py

--- userbot/modules/klon.py
+++ userbot/modules/klon.py
@@ -78,7 +78,7 @@ async def clone(event):
     await event.delete()
     await event.client.send_message(
       event.chat_id,
-      "`Hahahah, Owen Sayesinde Seni Çaldım.`",
+      "`Hahahah, Lavander Sayesinde Seni Çaldım.`",
       reply_to=reply_message
       )
 
@@ -102,7 +102,7 @@ async def revert(event):
         await bot(functions.account.UpdateProfileRequest(about=DEFAULT_BIO))
         await event.edit(f"`{DEFAULT_NAME}, hesabınız başarıyla eski haline döndürüldü!`")
     except AboutTooLongError:
-        srt_bio = "🎆 @OwenUserBot"
+        srt_bio = "🎆 @LavanUserBot"
         await bot(functions.account.UpdateProfileRequest(about=srt_bio))
         await event.edit("`Hesabınız başarıyla eski haline döndürüldü! Fakat bio'nuz çok uzun olduğu için hazır bio kullandım.`")
 
@@ -164,4 +164,4 @@ async def get_full_user(event):
 CmdHelp('klon').add_command('klon', LANG['K1'], LANG['K2']
 ).add_command('revert', None, LANG['K3']
 ).add_warning(LANG['K4']
-).add_info('🎆 Thx to @erdewbey').add()
+).add_info('🎆 Thx to @Ber4tbey').add()
